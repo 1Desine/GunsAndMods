3ad6d58a9a5d63a75f0a0377b5b0d18de2638e9f
Влад Наполов <desine.main@gmail.com>
2023-07-12 12:31:39 +0300

    Support any shooting mode

--- Assets/Gun/Scripts/BaseGun.cs
+++ Assets/Gun/Scripts/BaseGun.cs
@@ -11,8 +11,16 @@ public class BaseGun : MonoBehaviour {
     [SerializeField] protected Transform defaultScopePosition;
     [SerializeField] protected Transform attachedScopePosition;
 
+    protected int shootingModeSelected = 0;
+    protected List<ShootingMode> shootingModesList = new List<ShootingMode>();
+    protected class ShootingMode {
+        public bool singleAction;
+        public int amountOfShots;
+        public float nextShotDelay;
+    }
 
-    protected int fireRate;
+
+    protected int fireRate = 300;
     protected int clipSize;
 
     protected int clipAmmo;
@@ -20,6 +28,19 @@ public class BaseGun : MonoBehaviour {
 
     protected float lastShotTime;
 
+    protected bool shootTriggerIsPulled;
+
+
+    private void Awake() {
+        shootingModesList.Add(new ShootingMode() {
+            singleAction = true,
+            amountOfShots = 3,
+            nextShotDelay = 60f / fireRate,
+        });
+        Debug.Log(shootingModesList[0].singleAction);
+        Debug.Log(shootingModesList[0].amountOfShots);
+        Debug.Log(shootingModesList[0].nextShotDelay);
+    }
 
     private void OnEnable() {
         SetValuesInGunController();
@@ -37,20 +58,31 @@ public class BaseGun : MonoBehaviour {
     }
 
     private void Update() {
-        if (gunControllerSO.shootButtonDown) TryShoot();
-        if (gunControllerSO.reloadButtonDown) TryReload();
+        ManageShooting(gunControllerSO.shootButtonDown);
+
+        if(gunControllerSO.reloadButtonDown) TryReload();
     }
 
 
-    private void TryShoot() {
-        if (CanShoot() == false) return;
+    private void ManageShooting(bool shootButtonDown) {
+        if(shootButtonDown == false) {
+            shootTriggerIsPulled = false;
+            return;
+        }
+        ShootingMode mode = shootingModesList[shootingModeSelected];
 
-        lastShotTime = Time.time;
-        
-        Shoot();
+        bool canShoot = false;
+        if(Time.time - lastShotTime > 60f / fireRate) { // Rate of fire allows to shoot another round
+            canShoot = true;
+            if(mode.singleAction && shootTriggerIsPulled == true) { // Single action mode AND trigger was Not reset
+                canShoot = false;
+            }
+        }
+        if(canShoot) {
+            StartCoroutine(ShootingCoroutine(mode.amountOfShots, mode.nextShotDelay));
+        }
 
-        ManageAmmoAfterShot();
-        gunControllerSO.ClipAmmoChanged(clipAmmo);
+        shootTriggerIsPulled = shootButtonDown;
     }
 
     private void TryReload() {
@@ -58,6 +90,19 @@ public class BaseGun : MonoBehaviour {
         gunControllerSO.ClipAmmoChanged(clipAmmo);
     }
 
+
+    private IEnumerator ShootingCoroutine(int amountOfShots, float nextShotDelay) {
+        Shoot();
+        lastShotTime = Time.time;
+        ManageAmmoAfterShot();
+        gunControllerSO.ClipAmmoChanged(clipAmmo);
+
+        amountOfShots--;
+        if(amountOfShots > 0) {
+            yield return new WaitForSeconds(nextShotDelay);
+            StartCoroutine(ShootingCoroutine(amountOfShots, nextShotDelay));
+        }
+    }
 
 
 
@@ -77,7 +122,10 @@ public class BaseGun : MonoBehaviour {
     }
 
 
-
+    protected void SelectNextShootingMode() {
+        shootingModeSelected++;
+        shootingModeSelected %= shootingModesList.Count;
+    }
 
 
 
